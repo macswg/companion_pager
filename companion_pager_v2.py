3e333e09efb071cef85ee4a775dde21d788c9589
macswg <88906995+macswg@users.noreply.github.com>
2023-04-27 15:43:49 -0700

    companion_pager_v2.py is working. Still need to add some variables to help make updating different buttons easier, but it works as originally designed now.

--- companion_pager_v2.py
+++ companion_pager_v2.py
@@ -60,15 +60,15 @@ def buttonTitleUpdate(pageDict: dict, numOfIterations: int, startIndex: int, but
                 logger.info('Unable to increment the button title becuase it is not an int.')
 
 
-# TODO: update function to add button index (which will match the title not the companion button index)
-def buttonActionUpdate(pageDict: dict, numOfIterations: int, startIndex: int, scriptCue: int):
+def buttonActionUpdate(pageDict: dict, numOfIterations: int, startIndex: int, scriptIdStart: int, scriptCue: int):
     '''
     Updates the actions of multiple buttons and iterates up the RegisterID to match the appropriate
     button number.
 
     :pageDict: The dict of the page to update buttons on.
-    :numOfIterations: The number of buttons to update. 
+    :numOfIterations: The number of buttons to update.
     :startIndex: The index of the button to start on. This allows updating only a few buttons on the page.
+    :scriptIdStart: The script ID to start with.
     :scriptCue: The scriptCue to put in each preset. 1=Program 0=Preview
     '''
     for i, (key, value) in enumerate(pageDict.items()):
@@ -76,11 +76,11 @@ def buttonActionUpdate(pageDict: dict, numOfIterations: int, startIndex: int, sc
             break
         if i >= int(startIndex):
             for i in pageDict[key]:
-                i['options']['sidx'] = startIndex
+                i['options']['sidx'] = str(scriptIdStart)
                 i['options']['cidx'] = str(scriptCue)
-            sID = int(startIndex)
-            sID += 1
-            startIndex = str(sID)
+            strtIdx = int(scriptIdStart)
+            strtIdx += 1
+            scriptIdStart = str(strtIdx)
 
 
 
@@ -106,6 +106,19 @@ p1Buttons = fullConfigDict['config']['1']
 configOrigPgm = fullConfigDict['config']['1']['1']
 configOrigPst = fullConfigDict['config']['1']['9']
 
+# TODO: create variables to easily update indexes and copy numbers
+
+#
+#
+# variables to update to make the script easier to change
+#
+# button to start with
+# strtButton = 
+# number of copies to make
+# numOfButtons = 
+#
+#
+
 # make deep copy of dict of button '1' to subsequent buttons
 dictDeepCopy(configOrigPgm, p1Buttons, 7, 1)
 dictDeepCopy(configOrigPst, p1Buttons, 7, 9)
@@ -118,11 +131,8 @@ buttonTitleUpdate(p1Buttons, 8, 8, 1)
 buttonTitleUpdate(p1Buttons, 4, 16, 9)
 buttonTitleUpdate(p1Buttons, 4, 24, 9)
 
-
-#
 # actions
 #
-
 # dict of page 1 actions
 p1Actions = fullConfigDict['actions']['1']
 
@@ -137,69 +147,13 @@ dictDeepCopy(actionOrigPgm, p1Actions, 4, 16)
 dictDeepCopy(actionOrigPst, p1Actions, 4, 24)
 
 # update button actions
-buttonActionUpdate(p1Actions, 7, 0, 1) # Script Cue at 1 for PGM
-buttonActionUpdate(p1Actions, 7, 0, 0) # Script Cue at 0 for PVW
-buttonActionUpdate(p1Actions, 4, 16, 1) # Script Cue at 1 for PGM
-buttonActionUpdate(p1Actions, 4, 24, 0) # Script Cue at 0 for PVW
+buttonActionUpdate(p1Actions, 8, 0, 0, 1)  # Script Cue at 1 for PGM
+buttonActionUpdate(p1Actions, 8, 8, 0, 0)  # Script Cue at 0 for PVW
+buttonActionUpdate(p1Actions, 4, 16, 8, 1)  # Script Cue at 1 for PGM
+buttonActionUpdate(p1Actions, 4, 24, 8, 0)  # Script Cue at 0 for PVW
 
-
-# jsonActions = json.dumps(p1Actions, indent=4)
-# logger.debug(f'p1Actions = {jsonActions}')
-
-
-# buttonId = '1'
-# for i, (key, value) in enumerate(p1Buttons.items()):
-#     if i == 7:
-#         break
-#     p1Buttons[key]['text'] = buttonTitle
-#     buttonTitle = int(buttonTitle)
-#     buttonTitle += 1
-#     buttonTitle = str(buttonTitle)
-
-# for key, subdict in p1Buttons.items():
-#     intId = int(buttonId)
-#     intId += 1
-#     newId = str(intId)
-#     for i in range(5):
-#         p1Buttons[key]['text'] = newId
-
-# p1Buttons['2']['text'] = '2'
-# p1Buttons['3']['text'] = '3'
-
-# # all instances
-# instDict = fullConfigDict['instances']
-# # print(type(ogDict))
-# jsonData6 = json.dumps(instDict, indent=2)
-# logging.debug(jsonData6)
-
-# # actions
-# actDict = fullConfigDict['actions']['1']['1']
-# actListLabel, actListInst = actDict[0]['label'], actDict[0]['instance']
-# jsonData7 = json.dumps(actListLabel, indent=2)
-# jsonData8 = json.dumps(actListInst, indent=2)
-# logging.debug(jsonData7)
-
-# open new file and write updated JSON
-# with open(write_config_file) as newF:
-#     readNewFile = newF.readlines()
-# newStrForJson = readNewFile[0]
-# newConfigDict = json.loads(newStrForJson)
-# newConfigDict['config']['1']['1'] = ogDict
-
-
-# print(writeFile)
-# updatedStrForJson = writeFile[0]
-# newDict = json.loads(updatedStrForJson)
-# newDict['config']['1']['1'] = ogDict
-
-# updateStyle = newDict['config']['1']['1']
-# updateStyle = ogDict
-
-# print(type(updatedStrForJson[0]))
-
-# print(type(newDict))
-
-
+#
+# create JSON from dict
 newJSON = json.dumps(fullConfigDict)
 
 # writes to new file
